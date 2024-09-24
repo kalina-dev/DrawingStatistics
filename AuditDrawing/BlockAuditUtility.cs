b6eecf4070bbe1790467db9366494ef42423a753
Kalina Aleksandrova <Kalina Aleksandrova@STZ-LAP-ETM001>
2024-09-24 08:48:02 +0300

    Add project files.

---
+++ AuditDrawing/BlockAuditUtility.cs
@@ -0,0 +1,280 @@
+﻿using System.IO;
+using System.Collections;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+
+namespace DrawingStatistics
+{
+    public class BlockAuditUtility
+    {
+
+        [CommandMethod("AuditBlock")]
+        public void AuditBlock()
+        {
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            PromptKeywordOptions pko = new PromptKeywordOptions("Select Display Mode: ");
+            pko.Keywords.Add("Screen");
+            pko.Keywords.Add("TXT");
+            pko.Keywords.Add("CSV");
+            pko.Keywords.Add("HTML");
+            pko.AllowNone = true;
+
+            PromptResult res = edt.GetKeywords(pko);
+            string answer = res.StringResult;
+
+            switch (answer)
+            {
+                case "Screen":
+                    DisplayBlockAuditOnScreen();
+                    break;
+                case "TXT":
+                    WriteBlockAuditToTextFile();
+                    break;
+                case "CSV":
+                    WriteBlockAuditToCSVFile();
+                    break;
+                case "HTML":
+                    WriteBlockAuditToHTMLFile();
+                    break;
+            }
+        }
+
+        private void DisplayBlockAuditOnScreen()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                // Get all the Blocks
+                var result = GatherBlocksAndCounts();
+                if (result != null)
+                {                    
+                    ArrayList arBlocks = new ArrayList();
+                    int[] arCounts = null;
+                    int i = 0;
+
+                    arBlocks = (ArrayList) result[0];
+                    arCounts = (int[]) result[1];
+
+                    // Display the results
+                    edt.WriteMessage("\nList of Blocks found in the drawing: ");
+                    foreach (string blockname in arBlocks)
+                    {
+                        edt.WriteMessage("\nBlock: " + blockname + " = " + arCounts[i]);
+                        i += 1;
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private void WriteBlockAuditToTextFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                ArrayList result = GatherBlocksAndCounts();
+                if (result != null)
+                {
+                    ArrayList arBlocks = new ArrayList();
+                    int[] arCounts = null;
+                    int i = 0;
+
+                    arBlocks = (ArrayList)result[0];
+                    arCounts = (int[])result[1];
+
+                    PromptStringOptions pso = new PromptStringOptions("Enter TXT filename and location: ");
+                    PromptResult pr = edt.GetString(pso);
+                    string filename = pr.StringResult;
+
+                    if (filename != "")
+                    {
+                        using (StreamWriter file = new StreamWriter(filename))
+                        {
+                            // Write the results to the text file
+                            file.WriteLine("\nList of Blocks found in the drawing: ");
+
+                            foreach (string blockname in arBlocks)
+                            {
+                                file.WriteLine("\nBlock: " + blockname + " = " + arCounts[i]);
+                                i += 1;
+                            }                            
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private void WriteBlockAuditToCSVFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                ArrayList result = GatherBlocksAndCounts();
+                if (result != null)
+                {
+                    ArrayList arBlocks = new ArrayList();
+                    int[] arCounts = null;
+                    int i = 0;
+
+                    arBlocks = (ArrayList)result[0];
+                    arCounts = (int[])result[1];
+
+                    PromptStringOptions pso = new PromptStringOptions("Enter CSV filename and location: ");
+                    PromptResult pr = edt.GetString(pso);
+                    string filename = pr.StringResult;
+
+                    if (filename != "")
+                    {
+                        using (StreamWriter file = new StreamWriter(filename))
+                        {
+                            // Write the results to the text file
+                            file.WriteLine("List of Blocks found in the drawing: ");
+                            file.WriteLine("Block Name, Count");
+                            foreach (string blockname in arBlocks)
+                            {
+                                file.Write("\nBlock: " + blockname + "," + arCounts[i]);
+                                i += 1;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private void WriteBlockAuditToHTMLFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                ArrayList result = GatherBlocksAndCounts();
+                if (result != null)
+                {
+                    ArrayList arBlocks = new ArrayList();
+                    int[] arCounts = null;
+                    int i = 0;
+
+                    arBlocks = (ArrayList)result[0];
+                    arCounts = (int[])result[1];
+
+                    PromptStringOptions pso = new PromptStringOptions("Enter HTML filename and location: ");
+                    PromptResult pr = edt.GetString(pso);
+                    string filename = pr.StringResult;
+
+                    if (filename != "")
+                    {
+                        using (StreamWriter file = new StreamWriter(filename))
+                        {
+                            // Write the results to the HTML file
+                            file.WriteLine("<html>");
+                            file.WriteLine("<head></head>");
+                            file.WriteLine("<body>");
+                            file.WriteLine("<h2 style='background-color:yellow'>List of Blocks found in the drawing: </h2>");
+                            file.WriteLine("<table border=1>");
+                            file.WriteLine("<tr><td>Block Name</td><td>Count</td></tr>");
+                            foreach (string blockname in arBlocks)
+                            {
+                                file.Write("<tr><td>Block:" + blockname + " </td><td style='color:blue'>" + arCounts[i] + "</td></tr>");
+                                i += 1;
+                            }
+                            file.WriteLine("</table>");
+                            file.WriteLine("</body>");
+                            file.WriteLine("</html>");
+                        }
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+
+        private ArrayList GatherBlocksAndCounts()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor edt = doc.Editor;
+            ArrayList result = new ArrayList();
+
+            try
+            {
+                using (Transaction trans = db.TransactionManager.StartTransaction())
+                {
+                    // Get all the Blocks
+                    TypedValue[] tv = new TypedValue[1];
+                    tv.SetValue(new TypedValue((int)DxfCode.Start, "INSERT"), 0);
+                    PromptSelectionResult psr = edt.SelectAll(new SelectionFilter(tv));
+
+                    // Get all the unique Blocks and store in an ArrayList collection then sort the array
+                    string blockName = "";
+                    var blks = psr.Value.GetObjectIds();
+                    ArrayList blkCol = new ArrayList();
+                    int iCount = 0;
+                    foreach (ObjectId brId in blks)
+                    {
+                        BlockReference br = (BlockReference)trans.GetObject(brId, OpenMode.ForRead);
+                        blockName = br.Name.ToString();
+
+                        if (blkCol.IndexOf(blockName) == -1)
+                        {
+                            blkCol.Add(blockName);
+                            iCount += 1;
+                        }
+                    }
+                    blkCol.Sort();
+
+                    // Loop through the Block Collection and count the number of each block and store in the array
+                    int[] blkCount = new int[iCount];
+                    int i;
+                    foreach (ObjectId brId in blks)
+                    {
+                        BlockReference br = (BlockReference)trans.GetObject(brId, OpenMode.ForRead);
+                        blockName = br.Name.ToString();
+
+                        for (i = 0; i <= iCount - 1; i++)
+                        {
+                            if (blkCol[i].ToString() == blockName)
+                            {
+                                blkCount[i] += 1;
+                                break;
+                            }
+                        }
+                    }
+
+                    // Gather the results and return
+                    result.Add(blkCol);
+                    result.Add(blkCount);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+                return null;
+            }
+            return result;
+        }
+    }
+}
---
+++ AuditDrawing/DrawingAuditUtility.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.EditorInput;
+using System.IO;
+
+namespace DrawingStatistics
+{
+    public class DrawingAuditUtility
+    {
+        [CommandMethod("DrawingStatistics")]
+        public void AuditDrawingOptions()
+        {
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            PromptKeywordOptions pko = new PromptKeywordOptions("Select Display Mode: ");
+            pko.Keywords.Add("Screen");
+            pko.Keywords.Add("TXT");
+            pko.Keywords.Add("CSV");
+            pko.Keywords.Add("HTML");
+            pko.AllowNone = true;
+
+            PromptResult res = edt.GetKeywords(pko);
+            string answer = res.StringResult;
+
+            switch (answer) {
+                case "Screen":
+                    DisplayDrawingAuditOnScreen();
+                    break;
+                case "TXT":
+                    WriteDrawingAuditToTextFile();
+                    break;
+                case "CSV":
+                    WriteDrawingAuditToCSVFile();
+                    break;
+                case "HTML":
+                    WriteDrawingAuditToHTMLFile();
+                    break;
+            }
+        }
+
+        private void WriteDrawingAuditToHTMLFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                PromptStringOptions pso = new PromptStringOptions("Enter HTML filename and location: ");
+                PromptResult pr = edt.GetString(pso);
+                string filename = pr.StringResult;
+
+                if (filename != "")
+                {
+                    // Now, write the information to a file
+                    int lineCount, mtxCount, plCount, arcCount, blkCount;
+                    lineCount = GetEntityCount("LINE");
+                    mtxCount = GetEntityCount("MTEXT");
+                    plCount = GetEntityCount("LWPOLYLINE");
+                    arcCount = GetEntityCount("ARC");
+                    blkCount = GetEntityCount("INSERT");
+
+                    int totalCount = lineCount + mtxCount + plCount + arcCount + blkCount;
+
+                    using (StreamWriter file = new StreamWriter(filename))
+                    {
+                        // Write the Header
+                        file.WriteLine("<html>");
+                        file.WriteLine("<head></head>");
+                        file.WriteLine("<body>");
+                        file.WriteLine("<h2 style='background-color:yellow'>List of Objects found in the drawing:</h2>");
+                        file.WriteLine("<table border=1>");
+                        file.WriteLine("<tr>");
+                        file.WriteLine("<td style='color:blue'>Lines</td>");
+                        file.WriteLine("<td style='color:blue'>MTexts</td>");
+                        file.WriteLine("<td style='color:blue'>Polylines</td>");
+                        file.WriteLine("<td style='color:blue'>Arcs</td>");
+                        file.WriteLine("<td style='color:blue'>Blocks</td>");
+                        file.WriteLine("<td style='color:blue'>Total</td></tr>");
+                        file.WriteLine("</tr>");
+                        file.WriteLine("<tr>");
+                        file.WriteLine("<td>" + lineCount.ToString() + "</td>");
+                        file.WriteLine("<td>" + mtxCount.ToString() + "</td>");
+                        file.WriteLine("<td>" + plCount.ToString() + "</td>");
+                        file.WriteLine("<td>" + arcCount.ToString() + "</td>");
+                        file.WriteLine("<td>" + blkCount.ToString() + "</td>");
+                        file.WriteLine("<td>" + totalCount.ToString() + "</td>");
+                        file.WriteLine("</tr>");
+                        file.WriteLine("</table>");
+                        file.WriteLine("</body>");
+                        file.WriteLine("</html>");
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private void WriteDrawingAuditToCSVFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                PromptStringOptions pso = new PromptStringOptions("Enter CSV filename and location: ");
+                PromptResult pr = edt.GetString(pso);
+                string filename = pr.StringResult;
+
+                if (filename != "")
+                {
+                    // Now, write the information to a file
+                    int lineCount, mtxCount, plCount, arcCount, blkCount;
+                    lineCount = GetEntityCount("LINE");
+                    mtxCount = GetEntityCount("MTEXT");
+                    plCount = GetEntityCount("LWPOLYLINE");
+                    arcCount = GetEntityCount("ARC");
+                    blkCount = GetEntityCount("INSERT");
+
+                    int totalCount = lineCount + mtxCount + plCount + arcCount + blkCount;
+
+                    using (StreamWriter file = new StreamWriter(filename))
+                    {
+                        file.WriteLine("List of Objects found in the drawing: ");
+                        file.WriteLine("Lines, MTexts, Polylines, Arcs, Blocks, Total");
+                        file.WriteLine(lineCount.ToString() + "," + mtxCount.ToString() + "," + plCount.ToString() + "," + arcCount.ToString() + "," + blkCount.ToString() + "," + totalCount.ToString());
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private void WriteDrawingAuditToTextFile()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                PromptStringOptions pso = new PromptStringOptions("Enter TXT filename and location: ");
+                PromptResult pr = edt.GetString(pso);
+                string filename = pr.StringResult;
+
+                if (filename != "")
+                {
+                    // Now, write the information to a file
+                    int lineCount, mtxCount, plCount, arcCount, blkCount;
+                    lineCount = GetEntityCount("LINE");
+                    mtxCount = GetEntityCount("MTEXT");
+                    plCount = GetEntityCount("LWPOLYLINE");
+                    arcCount = GetEntityCount("ARC");
+                    blkCount = GetEntityCount("INSERT");
+
+                    int totalCount = lineCount + mtxCount + plCount + arcCount + blkCount;
+
+                    using (StreamWriter file = new StreamWriter(filename))
+                    {
+                        file.WriteLine("\nList of Objects found in the drawing: ");
+                        file.WriteLine("\nLines: " + lineCount.ToString());
+                        file.WriteLine("\nMTexts: " + mtxCount.ToString());
+                        file.WriteLine("\nPoylines: " + plCount.ToString());
+                        file.WriteLine("\nArcs: " + arcCount.ToString());
+                        file.WriteLine("\nBlocks: " + blkCount.ToString());
+                        file.WriteLine("\nTotal Objects Count: " + totalCount.ToString());
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        public void DisplayDrawingAuditOnScreen()
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            try
+            {
+                int lineCount, mtxCount, plCount, arcCount, blkCount;
+                lineCount = GetEntityCount("LINE");
+                mtxCount = GetEntityCount("MTEXT");
+                plCount = GetEntityCount("LWPOLYLINE");
+                arcCount = GetEntityCount("ARC");
+                blkCount = GetEntityCount("INSERT");
+
+                int totalCount = lineCount + mtxCount + plCount + arcCount + blkCount;
+
+                // Now, display the results
+                edt.WriteMessage("\nList of Objects found in the drawing: ");
+                edt.WriteMessage("\nLines: " + lineCount.ToString());
+                edt.WriteMessage("\nMTexts: " + mtxCount.ToString());
+                edt.WriteMessage("\nPoylines: " + plCount.ToString());
+                edt.WriteMessage("\nArcs: " + arcCount.ToString());
+                edt.WriteMessage("\nBlocks: " + blkCount.ToString());
+                edt.WriteMessage("\nTotal Objects Count: " + totalCount.ToString());
+            }
+            catch (System.Exception ex)
+            {
+                edt.WriteMessage("Error encountered: " + ex.Message);
+            }
+        }
+
+        private int GetEntityCount(string entityType)
+        {
+            // Get the Editor object
+            Editor edt = Application.DocumentManager.MdiActiveDocument.Editor;
+
+            // Get all the Objects in the specified entityType
+            TypedValue[] tv = new TypedValue[1];
+            tv.SetValue(new TypedValue((int)DxfCode.Start, entityType), 0);
+            SelectionFilter filter = new SelectionFilter(tv);
+            PromptSelectionResult ssPrompt = edt.SelectAll(filter);
+            int objCount = 0;
+            if (ssPrompt.Status == PromptStatus.OK)
+            {
+                SelectionSet ss = ssPrompt.Value;
+                objCount = ss.Count;
+            }
+            return objCount;
+        }
+
+        //private int GetLines()
+        //{
+        //    // Get all the Line Objects
+        //    TypedValue[] tv = new TypedValue[1];
+        //    tv.SetValue(new TypedValue((int)DxfCode.Start, "LINE"), 0);
+        //    SelectionFilter filter = new SelectionFilter(tv);
+        //    PromptSelectionResult ssPrompt = edt.SelectAll(filter);
+        //    int lineCount = 0;
+        //    if (ssPrompt.Status == PromptStatus.OK)
+        //    {
+        //        SelectionSet ssLines = ssPrompt.Value;
+        //        lineCount = ssLines.Count;
+        //    }
+
+        //    return lineCount;
+        //}
+
+        //private int GetMTexts()
+        //{
+        //    // Get all the MText Objects                
+        //    TypedValue[] tv = new TypedValue[1];
+        //    tv.SetValue(new TypedValue((int)DxfCode.Start, "MTEXT"), 0);
+        //    SelectionFilter filter = new SelectionFilter(tv);
+        //    filter = new SelectionFilter(tv);
+        //    PromptSelectionResult ssPrompt = edt.SelectAll(filter);
+        //    ssPrompt = edt.SelectAll(filter);
+        //    int mtxCount = 0;
+        //    if (ssPrompt.Status == PromptStatus.OK)
+        //    {
+        //        SelectionSet ssMTexts = ssPrompt.Value;
+        //        mtxCount = ssMTexts.Count;
+        //    }
+        //    return mtxCount;
+        //}
+
+        //private int GetPolylines()
+        //{
+        //    // Get all the Polyline Objects
+        //    TypedValue[] tv = new TypedValue[1];
+        //    tv.SetValue(new TypedValue((int)DxfCode.Start, "LWPOLYLINE"), 0);
+        //    SelectionFilter filter = new SelectionFilter(tv);
+        //    filter = new SelectionFilter(tv);
+        //    PromptSelectionResult ssPrompt = edt.SelectAll(filter);
+        //    ssPrompt = edt.SelectAll(filter);
+        //    int plCount = 0;
+        //    if (ssPrompt.Status == PromptStatus.OK)
+        //    {
+        //        SelectionSet ssPolyline = ssPrompt.Value;
+        //        plCount = ssPolyline.Count;
+        //    }
+        //    return plCount;
+        //}
+
+        //private int GetArcs()
+        //{
+        //    // Get all the Arc Objects
+        //    TypedValue[] tv = new TypedValue[1];
+        //    tv.SetValue(new TypedValue((int)DxfCode.Start, "ARC"), 0);
+        //    SelectionFilter filter = new SelectionFilter(tv);
+        //    filter = new SelectionFilter(tv);
+        //    PromptSelectionResult ssPrompt = edt.SelectAll(filter);
+        //    ssPrompt = edt.SelectAll(filter);
+        //    int arcCount = 0;
+        //    if (ssPrompt.Status == PromptStatus.OK)
+        //    {
+        //        SelectionSet ssArc = ssPrompt.Value;
+        //        arcCount = ssArc.Count;
+        //    }
+        //    return arcCount;
+        //}
+
+    }
+}
